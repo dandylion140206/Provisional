2243b4eed94621295bc75e49b23e1762c17ea270
dandylion140206 <dandylion140206@gmail.com>
2026-07-21 08:40:52 +0900

    37

--- visuals/screen_effects/screen_effect_parameter_reader.gd
+++ visuals/screen_effects/screen_effect_parameter_reader.gd
@@ -1,5 +1,9 @@
 class_name ScreenEffectParameterReader
+
 extends RefCounted
+
+
+const SHADER_FLOAT_SIGNIFICANT_DIGITS := 7
 
 
 static func read_all(shader: Shader) -> Array[ScreenEffectParameterDefinition]:
@@ -57,6 +61,7 @@ static func _read_parameter(
 			return null
 
 	parameter.default_value = RenderingServer.shader_get_parameter_default(shader_rid, parameter_id)
+
 	assert(parameter.default_value != null, "Shader parameter default value is null: %s" % parameter_id)
 
 	parameter.default_value = parameter.normalize_value(parameter.default_value)
@@ -78,13 +83,14 @@ static func _apply_range_hint(
 	assert(hint == PROPERTY_HINT_RANGE, "Numeric shader parameter requires hint_range: %s" % parameter.id)
 
 	var range_parts := hint_string.split(",")
+
 	assert(range_parts.size() >= 2, "Invalid hint_range: %s" % parameter.id)
 
-	parameter.min_value = range_parts[0].to_float()
-	parameter.max_value = range_parts[1].to_float()
+	parameter.min_value = _clean_shader_float(range_parts[0].to_float())
+	parameter.max_value = _clean_shader_float(range_parts[1].to_float())
 
 	if range_parts.size() >= 3:
-		parameter.step = range_parts[2].to_float()
+		parameter.step = _clean_shader_float(range_parts[2].to_float())
 	elif parameter.kind == ScreenEffectParameterDefinition.Kind.INTEGER:
 		parameter.step = 1.0
 	else:
@@ -94,11 +100,22 @@ static func _apply_range_hint(
 	assert(parameter.step > 0.0, "Parameter step must be greater than zero: %s" % parameter.id)
 
 
+static func _clean_shader_float(value: float) -> float:
+	if value == 0.0:
+		return 0.0
+
+	var exponent := floorf(log(absf(value)) / log(10.0))
+	var scale := pow(10.0, SHADER_FLOAT_SIGNIFICANT_DIGITS - 1 - exponent)
+
+	return round(value * scale) / scale
+
+
 static func _apply_enum_hint(
 	parameter: ScreenEffectParameterDefinition,
 	hint_string: String,
 ) -> void:
 	var option_entries := hint_string.split(",")
+
 	assert(not option_entries.is_empty(), "Enum options must not be empty: %s" % parameter.id)
 
 	for index in option_entries.size():
